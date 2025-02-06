b24ca91f5f610d3cf7d3b3f64dd85f161cb26909
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-06 21:06:18 +0000

    Real experiment config

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python benchmark.py --env jbw --timesteps 1000001 --subsample 10000 --seeds 5
-python benchmark.py --env forager --timesteps 1000001 --subsample 10000 --seeds 5
+python benchmark.py --env jbw --timesteps 10000001 --subsample 10000 --seeds 5
+python benchmark.py --env forager --timesteps 10000001 --subsample 10000 --seeds 5
 python plot.py
