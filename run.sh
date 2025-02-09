41cb0e1b745e8254d4be0f9d531d7b5e5a8eac8d
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-09 06:38:27 +0000

    New run script

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-python benchmark.py --env jbw --timesteps 5000000 --subsample 10000 --seed 0
-python benchmark.py --env jbw --timesteps 5000000 --subsample 10000 --seed 1
-python benchmark.py --env jbw --timesteps 5000000 --subsample 10000 --seed 2
-python benchmark.py --env jbw --timesteps 5000000 --subsample 10000 --seed 3
-python benchmark.py --env jbw --timesteps 5000000 --subsample 10000 --seed 4
-python benchmark.py --env forager --timesteps 5000000 --subsample 10000 --seed 0
-python benchmark.py --env forager --timesteps 5000000 --subsample 10000 --seed 1
-python benchmark.py --env forager --timesteps 5000000 --subsample 10000 --seed 2
-python benchmark.py --env forager --timesteps 5000000 --subsample 10000 --seed 3
-python benchmark.py --env forager --timesteps 5000000 --subsample 10000 --seed 4
+python benchmark.py --env forager --timesteps 2000000 --subsample 10000 --seed 0
+python benchmark.py --env forager --timesteps 2000000 --subsample 10000 --seed 1
+python benchmark.py --env forager --timesteps 2000000 --subsample 10000 --seed 2
+python benchmark.py --env forager --timesteps 2000000 --subsample 10000 --seed 3
+python benchmark.py --env forager --timesteps 2000000 --subsample 10000 --seed 4
+python benchmark.py --env jbw --timesteps 2000000 --subsample 10000 --seed 0
+python benchmark.py --env jbw --timesteps 2000000 --subsample 10000 --seed 1
+python benchmark.py --env jbw --timesteps 2000000 --subsample 10000 --seed 2
+python benchmark.py --env jbw --timesteps 2000000 --subsample 10000 --seed 3
+python benchmark.py --env jbw --timesteps 2000000 --subsample 10000 --seed 4
 python plot.py
