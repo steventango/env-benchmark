87750692a70e87e278c15bec72f74f05ad38c297
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-25 07:17:41 +0000

    Enhance plotting functions with custom color palette and improved layout

--- plot.py
+++ plot.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+gdm_colors = ["#4285f4", "#ff902a", "#34a853", "#ea4335", "#fbbc04", "#2daeb8"]
+
 
 def main():
     env_mapping = {
@@ -33,18 +35,20 @@ def load_df(csvs: Iterable[Path], env_mapping: dict):
         df["memory"] /= 1e9
         dfs.append(df)
     df = pd.concat(dfs)
+    df = df.sort_values("env")
     return df
 
 
 def plot_wall_time(df: pd.DataFrame):
-    plt.figure()
+    plt.figure(figsize=(3, 2))
 
-    sns.lineplot(data=df, x="step", y="wall_time", hue="env")
+    sns.lineplot(data=df, x="step", y="wall_time", hue="env", palette=gdm_colors, legend=False)
     sns.lineplot(
         data=df,
         x="step",
         y="wall_time",
         hue="env",
+        palette=gdm_colors,
         alpha=0.4,
         units="seed",
         estimator=None,
@@ -54,22 +58,31 @@ def plot_wall_time(df: pd.DataFrame):
     sns.despine()
     plt.xlabel("Timestep")
     plt.ylabel("Wall time (s)")
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
-    plt.legend(title=None)
+    plt.gca().ticklabel_format(axis="x", style="sci", scilimits=(0, 0), useMathText=True)
+
+    for line, label in zip(plt.gca().get_lines(), df["env"].unique()):
+        y = line.get_ydata()[-1]
+        w = df["step"].max() - df["step"].min()
+        h = df["wall_time"].max() - df["wall_time"].min()
+        plt.text(
+            df["step"].max() - 0.1 * w, y + 0.1 * h, label, color=line.get_color(), fontsize=10, va="center", ha="right"
+        )
+
     plt.tight_layout()
     plt.savefig("results/benchmark_wall_time.pdf")
     plt.savefig("results/benchmark_wall_time.jpg")
 
 
 def plot_memory(df: pd.DataFrame):
-    plt.figure()
+    plt.figure(figsize=(3, 2))
 
-    sns.lineplot(data=df, x="step", y="memory", hue="env")
+    sns.lineplot(data=df, x="step", y="memory", hue="env", palette=gdm_colors, legend=False)
     sns.lineplot(
         data=df,
         x="step",
         y="memory",
         hue="env",
+        palette=gdm_colors,
         alpha=0.4,
         units="seed",
         estimator=None,
@@ -79,16 +92,24 @@ def plot_memory(df: pd.DataFrame):
     sns.despine()
     plt.xlabel("Timestep")
     plt.ylabel("Memory (GB)")
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
-    plt.legend(title=None)
+    plt.gca().ticklabel_format(axis="x", style="sci", scilimits=(0, 0), useMathText=True)
+
+    for line, label in zip(plt.gca().get_lines(), df["env"].unique()):
+        y = line.get_ydata()[-1]
+        w = df["step"].max() - df["step"].min()
+        h = df["memory"].max() - df["memory"].min()
+        plt.text(
+            df["step"].max() - 0.1 * w, y + 0.1 * h, label, color=line.get_color(), fontsize=10, va="center", ha="right"
+        )
+
     plt.tight_layout()
     plt.savefig("results/benchmark_memory.pdf")
     plt.savefig("results/benchmark_memory.jpg")
 
 
 def plot_sps(df_agg: pd.DataFrame):
-    plt.figure()
-    sns.barplot(data=df_agg, x="env", y="sps", hue="env", capsize=0.2)
+    plt.figure(figsize=(3, 2))
+    sns.barplot(data=df_agg, x="env", y="sps", hue="env", palette=gdm_colors, capsize=0.2)
     sns.despine()
     plt.xlabel("Environment")
     plt.ylabel("Steps per second")
